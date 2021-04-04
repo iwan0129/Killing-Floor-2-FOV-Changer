86a147fa989dc30962310e6060d7bb4d4fa4e1bf
Iwan0129 <uzunow0129@gmail.com>
2021-04-04 17:19:28 +0300

    Small Code Changes

--- Killing Floor 2 FOV Changer/Source.py
+++ Killing Floor 2 FOV Changer/Source.py
@@ -8,30 +8,26 @@ narrow_fov = 'AspectRatioAxisConstraint=AspectRatio_MaintainXFOV\n'
 def find_index(searchPattern, data):
     result = [value for value in data if searchPattern in value];
 
-    if (result):
-        return data.index(result[0]);
-    else:
-        return 0;
+    return data.index(result[0]) if result else 0;
 pass;
 
 def read_file(path):
     with open(kfengine_path, 'r') as file:
         data = file.readlines();
         return data;
-    pass;       
 pass;
 
 def write_file(path, data):
     with open(path, 'w') as file:
         file.writelines(data);
-    pass;
 pass;
 
 if (path.exists(kfengine_path)):
     data = read_file(kfengine_path);
     fovIndex = find_index("AspectRatioAxisConstraint", data);
 
-    print('Enter 1 for Wide FOV\nEnter 2 for Narrow FOV\n');
+    print("Enter 1 for Wide FOV\nEnter 2 for Narrow FOV\n");
+
     fovType = int(input());
 
     if (fovType == 1):
@@ -44,4 +40,5 @@ if (path.exists(kfengine_path)):
     write_file(kfengine_path, data);
 
     print("\nDone. You Can Close The Program Now\n");
-    pass;
+else:
+    print("Unable To Find KFEngine.ini");
